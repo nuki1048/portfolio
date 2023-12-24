fe7dc3a57f41e0bafdfa29d1892a36d894e3799c
nuki1048 <chabaniuk.nik@gmail.com>
2023-12-24 19:24:47 +0200

    feat: add styles and icons to project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -13,8 +13,47 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      boxShadow: {
+        md: '0px 2px 2px 0px rgba(0, 0, 0, 0.06), 0px 4px 3px 0px rgba(0, 0, 0, 0.07)',
+        lg: '0px 10px 8px 0px rgba(0, 0, 0, 0.04), 0px 4px 3px 0px rgba(0, 0, 0, 0.10)',
+        '2xl': '0px 25px 25px 0px rgba(0, 0, 0, 0.15)',
+      },
+
+      colors: {
+        'gray-light': {
+          DEFAULT: '#FFF',
+          50: '#f9fafb',
+          100: '#f3f4f6',
+          200: '#e5e7eb',
+          300: '#d1d5db',
+          400: '#9ca3af',
+          500: '#6b7280',
+          600: '#4b5563',
+          700: '#374151',
+          800: '#1f2937',
+          900: '#111827',
+          950: '#030712',
+        },
+        'gray-dark': {
+          DEFAULT: '#030712',
+          50: '#111827',
+          100: '#1f2937',
+          200: '#374151',
+          300: '#4b5563',
+          400: '#6b7280',
+          500: '#9ca3af',
+          600: '#d1d5db',
+          700: '#e5e7eb',
+          800: '#f3f4f6',
+          900: '#f9fafb',
+          950: '#ffffff',
+        },
+        emerald: {
+          DEFAULT: '#10b981',
+        },
+      },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
